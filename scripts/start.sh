931ab7be8e9cfa618272868e43740ff2b1e88d4e
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2020-02-15 08:05:10 -0700

    create volumes in local dir (scripts)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # run IPFS
-docker run -d --name ipfs_host -e IPFS_PROFILE=server -v ipfs_staging:/export -v ipfs_data:/data/ipfs -p 4001:4001 -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 ipfs/go-ipfs:latest
+docker run -d --name ipfs_host -e IPFS_PROFILE=server -v $PWD/ipfs_staging:/export -v $PWD/ipfs_data:/data/ipfs -p 4001:4001 -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 ipfs/go-ipfs:latest
